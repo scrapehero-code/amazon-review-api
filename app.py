1a4300c06f2bb02f59e8dfe3c884b90a985c4e3b
Alyosha Romanov <tato.romanov@gmail.com>
2023-11-26 22:29:49 +0100

    Only load one page, but with specified page. Fix for NoneType error.

--- app.py
+++ app.py
@@ -7,62 +7,57 @@ app = Flask(__name__)
 extractor = selectorlib.Extractor.from_yaml_file('selectors.yml')
 
 def scrape(url):
-    all_reviews = []
-    while url:
+    headers = {
+        'authority': 'www.amazon.com',
+        'pragma': 'no-cache',
+        'cache-control': 'no-cache',
+        'dnt': '1',
+        'upgrade-insecure-requests': '1',
+        'user-agent': 'Mozilla/5.0 (X11; CrOS x86_64 8172.45.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.64 Safari/537.36',
+        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+        'sec-fetch-site': 'none',
+        'sec-fetch-mode': 'navigate',
+        'sec-fetch-dest': 'document',
+        'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
+    }
 
-        headers = {
-            'authority': 'www.amazon.com',
-            'pragma': 'no-cache',
-            'cache-control': 'no-cache',
-            'dnt': '1',
-            'upgrade-insecure-requests': '1',
-            'user-agent': 'Mozilla/5.0 (X11; CrOS x86_64 8172.45.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.64 Safari/537.36',
-            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-            'sec-fetch-site': 'none',
-            'sec-fetch-mode': 'navigate',
-            'sec-fetch-dest': 'document',
-            'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
-        }
-
-        # Download the page using requests
-        print("Downloading %s"%url)
-        r = requests.get(url, headers=headers)
-        # Simple check to check if page was blocked (Usually 503)
-        if r.status_code > 500:
-            if "To discuss automated access to Amazon data please contact" in r.text:
-                print("Page %s was blocked by Amazon. Please try using better proxies\n"%url)
+    # Download the page using requests
+    print("Downloading %s"%url)
+    r = requests.get(url, headers=headers)
+    # Simple check to check if page was blocked (Usually 503)
+    if r.status_code > 500:
+        if "To discuss automated access to Amazon data please contact" in r.text:
+            print("Page %s was blocked by Amazon. Please try using better proxies\n"%url)
+        else:
+            print("Page %s must have been blocked by Amazon as the status code was %d"%(url,r.status_code))
+        return None
+    # Pass the HTML of the page and create
+    data = extractor.extract(r.text,base_url=url)
+    reviews = []
+    for r in data['reviews']:
+        r['rating'] = int(float(r['title'].split(' out of')[0]))
+        r['title'] = r['title'].split(' out of 5 stars ')[-1]
+        r['product'] = data['product_title']
+        r['url'] = url
+        if r['found_helpful'] is None:
+            r['found_helpful'] = 0
+        elif 'One person found this helpful' in r['found_helpful']:
+            r['found_helpful'] = 1
+        elif 'people found this helpful' in r['found_helpful']:
+            r['found_helpful'] = int(r['found_helpful'].split()[0])
+        else:
+            r['found_helpful'] = 0
+        if 'verified_purchase' in r and r['verified_purchase'] is not None:
+            if 'Verified Purchase' in r['verified_purchase']:
+                r['verified_purchase'] = True
             else:
-                print("Page %s must have been blocked by Amazon as the status code was %d"%(url,r.status_code))
-            return None
-        # Pass the HTML of the page and create 
-        data = extractor.extract(r.text,base_url=url)
-        reviews = []
-        for r in data['reviews']:
-            r['rating'] = int(float(r['title'].split(' out of')[0]))
-            r['title'] = r['title'].split(' out of 5 stars ')[-1]
-            r['product'] = data['product_title']
-            r['url'] = url
-            if r['found_helpful'] is None:
-                r['found_helpful'] = 0
-            elif 'One person found this helpful' in r['found_helpful']:
-                r['found_helpful'] = 1
-            elif 'people found this helpful' in r['found_helpful']:
-                r['found_helpful'] = int(r['found_helpful'].split()[0])
-            else:
-                r['found_helpful'] = 0
-            if 'verified_purchase' in r:
-                if 'Verified Purchase' in r['verified_purchase']:
-                    r['verified_purchase'] = True
-                else:
-                    r['verified_purchase'] = False
-            date_posted = r['date'].split('on ')[-1]
-            if r['images']:
-                r['images'] = "\n".join(r['images'])
-            r['date'] = dateparser.parse(date_posted).strftime('%d %b %Y')
-            reviews.append(r)
-        all_reviews.extend(reviews)
-        url = data.get('next_page')
-    data['reviews'] = all_reviews
+                r['verified_purchase'] = False
+        date_posted = r['date'].split('on ')[-1]
+        if r['images']:
+            r['images'] = "\n".join(r['images'])
+        r['date'] = dateparser.parse(date_posted).strftime('%d %b %Y')
+        reviews.append(r)
+    data['reviews'] = reviews
     histogram = {}
     for h in data['histogram']:
         histogram[h['key']] = h['value']
@@ -74,6 +69,12 @@ def scrape(url):
 @app.route('/')
 def api():
     url = request.args.get('url',None)
+    if request.args.get('pageNumber',None) is None:
+        url += '&pageNumber=1'
+    elif int(request.args.get('pageNumber',None)) <= 10:
+        url += '&pageNumber='+request.args.get('pageNumber',None)
+    else:
+        return json.dumps({'error': 'Page number should be less than or equal to 10'}, indent=2), 400, {'Content-Type': 'application/json; charset=utf-8'}
     if url:
         data = scrape(url)
         return json.dumps(data, indent=2), 200, {'Content-Type': 'application/json; charset=utf-8'}
